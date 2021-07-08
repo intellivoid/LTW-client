05aa6138785b64b8f51998f94cebc667f2266f4f
Aliwoto <aminnimaj@gmail.com>
2021-07-08 17:29:28 +0000

    fixes _flat problems in InitializeComponents method
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- LTW/SandBoxes/ErrorSandBoxes/ProfileWrongSandBox.cs
+++ LTW/SandBoxes/ErrorSandBoxes/ProfileWrongSandBox.cs
@@ -24,83 +24,83 @@ using GUISharp.SandBox.ErrorSandBoxes;
 
 namespace LTW.SandBoxes.ErrorSandBoxes
 {
-    public sealed partial class ProfileWrongSandBox : ErrorSandBox
-    {
-        //-------------------------------------------------
-        #region Constant's Region
+	public sealed partial class ProfileWrongSandBox : ErrorSandBox
+	{
+		//-------------------------------------------------
+		#region Constant's Region
 		public const string LEFT_BABYLONIA_ENTRANCE		= "f_080320212007";
 		public const string RIGHT_BABYLONIA_ENTRANCE	= "f_080320212008";
 		public const string LINE_BABYLONIA_ENTRANCE		= "f_080320212009";
-        /// <summary>
-        /// The name of MessageLabel1 in the Resources without _name,
-        /// </summary>
-        public const string SandBoxLabel1NameInRes = "SandBoxLabel1";
-        /// <summary>
-        /// The name of MessageLabel2 in the Resources without _name,
-        /// </summary>
-        public const string SandBoxLabel2NameInRes = "SandBoxLabel2";
-        /// <summary>
-        /// The name of Button1 in the Resources without _name,
-        /// </summary>
-        public const string SandBoxButton1NameInRes = "SandBoxButton1";
-        /// <summary>
-        /// The background Image key in the <see cref="GraphicElement.MyRes"/>.
-        /// </summary>
-        public const string SandBoxBackGNameInRes = "BackGName";
-        #endregion
-        //-------------------------------------------------
-        #region Properties Region
-        /// <summary>
-        /// the title flat element of this sandbox.
-        /// </summary>
-        public FlatElement TitleElement { get; private set; }
-        /// <summary>
-        /// the body element of this sandbox.
-        /// </summary>
-        public FlatElement BodyElement { get; private set; }
-        /// <summary>
-        /// the back button of this sandbox.
-        /// </summary>
-        public ButtonElement BackButton { get; private set; }
-        public Texture2D LeftTexture { get; private set; }
-        public Texture2D RightTexture { get; private set; }
-        public Texture2D LineTexture { get; private set; }
-        public Point LeftTextureRealLocation { get; private set; }
-        public Point LineTextureRealLocation { get; private set; }
-        public Point RightTextureRealLocation { get; private set; }
-        public Rectangle LeftTextureRectangle { get; private set; }
-        public Rectangle LineTextureRectangle { get; private set; }
-        public Rectangle RightTextureRectangle { get; private set; }
-        public bool ClosedForRetry
-        {
-            get
-            {
-                return ClosedByMe;
-            }
-            set
-            {
-                ClosedByMe = value;
-            }
-        }
-        #endregion
-        //-------------------------------------------------
-        #region field's Region
+		/// <summary>
+		/// The name of MessageLabel1 in the Resources without _name,
+		/// </summary>
+		public const string SandBoxLabel1NameInRes = "SandBoxLabel1";
+		/// <summary>
+		/// The name of MessageLabel2 in the Resources without _name,
+		/// </summary>
+		public const string SandBoxLabel2NameInRes = "SandBoxLabel2";
+		/// <summary>
+		/// The name of Button1 in the Resources without _name,
+		/// </summary>
+		public const string SandBoxButton1NameInRes = "SandBoxButton1";
+		/// <summary>
+		/// The background Image key in the <see cref="GraphicElement.MyRes"/>.
+		/// </summary>
+		public const string SandBoxBackGNameInRes = "BackGName";
+		#endregion
+		//-------------------------------------------------
+		#region Properties Region
+		/// <summary>
+		/// the title flat element of this sandbox.
+		/// </summary>
+		public FlatElement TitleElement { get; private set; }
+		/// <summary>
+		/// the body element of this sandbox.
+		/// </summary>
+		public FlatElement BodyElement { get; private set; }
+		/// <summary>
+		/// the back button of this sandbox.
+		/// </summary>
+		public ButtonElement BackButton { get; private set; }
+		public Texture2D LeftTexture { get; private set; }
+		public Texture2D RightTexture { get; private set; }
+		public Texture2D LineTexture { get; private set; }
+		public Point LeftTextureRealLocation { get; private set; }
+		public Point LineTextureRealLocation { get; private set; }
+		public Point RightTextureRealLocation { get; private set; }
+		public Rectangle LeftTextureRectangle { get; private set; }
+		public Rectangle LineTextureRectangle { get; private set; }
+		public Rectangle RightTextureRectangle { get; private set; }
+		public bool ClosedForRetry
+		{
+			get
+			{
+				return ClosedByMe;
+			}
+			set
+			{
+				ClosedByMe = value;
+			}
+		}
+		#endregion
+		//-------------------------------------------------
+		#region field's Region
 
-        #endregion
-        //-------------------------------------------------
-        #region Constructor's Region
-        internal ProfileWrongSandBox() : base()
-        {
-            InitializeComponent();
-        }
-        #endregion
-        //-------------------------------------------------
-        #region Destructor's Region
-        ~ProfileWrongSandBox()
-        {
-            return;
-        }
-        #endregion
-        //-------------------------------------------------
-    }
+		#endregion
+		//-------------------------------------------------
+		#region Constructor's Region
+		internal ProfileWrongSandBox() : base()
+		{
+			InitializeComponent();
+		}
+		#endregion
+		//-------------------------------------------------
+		#region Destructor's Region
+		~ProfileWrongSandBox()
+		{
+			return;
+		}
+		#endregion
+		//-------------------------------------------------
+	}
 }
